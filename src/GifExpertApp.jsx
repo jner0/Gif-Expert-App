b33f2970d539467d523b501036524e95122d6712
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-05 23:14:30 -0500

    Probando hooks para categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState([ 'Peaky Blinders', 'The Sopranos' ]);
+    console.log(categories);
+
   return (
     <>
-
+        {/* titulo */}
         <h1>Gif Expert App</h1>
+
+        {/* Input */}
+
+        {/* Listado de Gifs */}
+
+        <ol>
+
+            { categories.map( category => {
+                return <li key = { category }> { category } </li>
+            }) }
+            
+        </ol>
+
+            {/* Gif Items */}
     
     </>
   )
