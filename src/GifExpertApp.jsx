61673cf42e0696b9821a26c3ea8a23b6661ac35b
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-07 22:52:11 -0500

    Empezando a consumir API de gifs

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,7 +4,7 @@ import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState([ 'Peaky Blinders', 'The Sopranos' ]);
+    const [categories, setCategories] = useState([ 'Peaky Blinders']);
     
     const  onAddCategory = ( newCategory ) => {
 
