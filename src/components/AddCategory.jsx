0536e7c35e05d725034e10345d282a45c5ae9157
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-14 23:04:32 -0500

    Tests en AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 export const AddCategory = ( { onNewCategory } ) => {
 
@@ -9,6 +10,7 @@ export const AddCategory = ( { onNewCategory } ) => {
   }
 
   const onSubmit = ( event ) => {
+    console.log('Hola Mundo desde onSubmit');
     event.preventDefault();
     if(inputValue.trim().length <= 1) return;
 
@@ -20,7 +22,7 @@ export const AddCategory = ( { onNewCategory } ) => {
 
   return (
 
-    <form onSubmit={ (event ) => onSubmit(event) }>
+    <form onSubmit={ (event ) => onSubmit(event)} aria-label="form" >
         <input 
             type="text"
             placeholder="Buscar Gif"
@@ -31,4 +33,8 @@ export const AddCategory = ( { onNewCategory } ) => {
     </form>
     
   )
+}
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired
 }
---
+++ tests/components/AddCategory.test.jsx
@@ -0,0 +1,35 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { AddCategory } from "../../src/components/AddCategory";
+
+describe('Pruebas en AddCategory', () => { 
+
+    test('Debe de cambiar el valor de la caja de texto', () => { 
+
+            render( <AddCategory onNewCategory={ () => {} } /> );
+            const input = screen.getByRole('textbox');
+
+            fireEvent.input( input, { target: { value: 'Saitama' }} );
+
+            expect( input.value ).toBe( 'Saitama' );
+            //screen.debug(); //me ayuda a ver lo que esta evaluando
+
+     });
+
+     test('Debe de llamar onNewCategory si el input tiene un valor', () => { 
+
+        const inputValue = 'Saitama';
+
+        render( <AddCategory onNewCategory={ () => {} } /> );
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input( input, { target: { value: inputValue }} );
+        fireEvent.submit( form );
+        //screen.debug();
+        expect( input.value ).toBe('');
+
+     });
+
+});
+
