690a61e1e746c129e493b9ab063cfa1ee3f6071d
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-16 23:57:41 -0500

    Final Commit en test

---
+++ tests/GifExpertApp.test.jsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import { GifExpertApp } from "../src/GifExpertApp";
+
+describe('Pruebas en el componente', () => { 
+
+    test('should ', () => { 
+
+        render(<GifExpertApp/>); 
+
+     });
+
+ })
